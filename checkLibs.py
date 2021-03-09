35f7460e4837799175223f84c484ea4f2f55686b
cluffja <joshua.cluff@nov.com>
2021-03-09 09:58:33 -0600

    Add check file, add requirements.txt

--- checkLibs.py
+++ checkLibs.py
@@ -1,7 +1,7 @@
 from stdlib_list import stdlib_list
 if __name__ == "__main__":
     libraries = stdlib_list("3.6")
-    f = open("requirements.txt", 'r')
+    f = open("check_requirements.txt", 'r')
     print("Libraries that are not in the standard library are:")
     for lib in f:
         lib = ''.join(e for e in lib if e.isalnum())
