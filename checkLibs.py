cc4c5fbc29de8e05b61a3c1e3a4e1e704ce563ca
Cluff, Joshua A <Joshua.Cluff@nov.com>
2021-03-09 09:53:07 -0600

    Initial commit

---
+++ checkLibs.py
@@ -0,0 +1,9 @@
+from stdlib_list import stdlib_list
+if __name__ == "__main__":
+    libraries = stdlib_list("3.6")
+    f = open("requirements.txt", 'r')
+    print("Libraries that are not in the standard library are:")
+    for lib in f:
+        lib = ''.join(e for e in lib if e.isalnum())
+        if not lib in libraries:
+            print(lib)
